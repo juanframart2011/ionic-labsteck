0c1b5a27cfcf2ee8ca72ffecc4d66e7c33a20de7
Juan Rogelio Franco Martinez <juanframart2011@gmail.com>
2021-02-03 22:21:28 -0600

    estructura interface

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -11,11 +11,15 @@ import { UserService } from '../service/user.service';
 })
 export class RegisterPage implements OnInit {
 
-  user:User;
+  countryCurrent:number;
+  regionCurrent:number;
+
+  user:User = {};
+  
   countrys:Country[] = [];
   states:State[] = [];
 
-  constructor( private userService:UserService ) { }
+  constructor( private userService:UserService ) {}
 
   ngOnInit() {
 
@@ -35,5 +39,54 @@ export class RegisterPage implements OnInit {
     this.userService.getState( "MX" ).subscribe(states => {
             this.states = states;
         });
+  }
+
+  register(){
+
+    this.user.enable = false;
+    this.user.createdBy = "manuel.perez@labsteck.com";
+    this.user.updatedBy = "manuel.perez@labsteck.com";
+    console.log( this.countryCurrent );
+    console.log( this.regionCurrent );
+
+    let ubicacion = {
+      x: 46.5,
+      y: 46.5
+    }
+
+    let address = {
+      country: this.countrys[this.countryCurrent],
+      region:         this.states[this.regionCurrent],
+      ciudad:         "asdfasdf",
+      direccion:      "asdfa adfas",
+      numeroExterior: "8",
+      numeroInterior: "S/N",
+      colonia:        "asdfaf",
+      telefono:       "qwerqwe",
+      ubicacion:      ubicacion,
+      cp:             "2242"
+    }
+
+    /*country?:        Country;
+    region?:         State;
+    ciudad?:         string;
+    direccion?:      string;
+    numeroExterior?: string;
+    numeroInterior?: string;
+    colonia?:        string;
+    telefono?:       string;
+    ubicacion?:      Ubicacion;
+    cp?:             string;
+
+    this.user.address.country = this.countrys[this.countryCurrent];
+    this.user.address.region = this.states[this.regionCurrent];*/
+    /*
+    this.user.address.ubicacion.x = 46.5;
+    this.user.address.ubicacion.y = 46.5;
+    this.user.address.cp = "2242";*/
+
+    this.user.address = address;
+
+    console.log( "user register() => ", this.user );
   }
 }
